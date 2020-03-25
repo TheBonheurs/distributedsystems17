3ae47831fe257192db1282954d5942adb5e26f96
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-03-25 12:48:30 +0100

    Rename DynamoRoutes to ExternalRoutes

--- src/main/scala/InternalServer.scala
+++ src/main/scala/InternalServer.scala
@@ -28,7 +28,7 @@ class InternalServer(context: ActorContext[InternalServer.Command], valueReposit
   implicit val classicActorSystem: actor.ActorSystem = context.system.toClassic
   implicit val materializer: Materializer = Materializer(classicActorSystem)
 
-  val routes = new DynamoRoutes(valueRepository)
+  val routes = new ExternalRoutes(valueRepository)
 
   var started = false
   var binding: ServerBinding = _;
