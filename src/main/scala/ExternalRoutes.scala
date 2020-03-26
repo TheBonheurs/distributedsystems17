d107a0b1d98fdb9831dcad49583ceed9b3a45cce
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-03-26 10:24:00 +0100

    ExternalRoutes no longer needs to extend SprayJsonSupport

--- src/main/scala/ExternalRoutes.scala
+++ src/main/scala/ExternalRoutes.scala
@@ -1,5 +1,4 @@
 import akka.actor.typed.{ActorRef, ActorSystem}
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
@@ -8,9 +7,8 @@ import akka.util.Timeout
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-class ExternalRoutes(buildValueRepository: ActorRef[ValueRepository.Command])(implicit system: ActorSystem[_]) extends SprayJsonSupport {
+class ExternalRoutes(buildValueRepository: ActorRef[ValueRepository.Command])(implicit system: ActorSystem[_]) {
   import JsonSupport._
-
   import akka.actor.typed.scaladsl.AskPattern._
 
   // asking someone requires a timeout and a scheduler, if the timeout hits without response
