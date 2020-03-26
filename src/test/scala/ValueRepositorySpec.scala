6f233e4b796902a8a9be5da360608751b7a30c7c
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-03-26 14:29:41 +0100

    Improve ValueRepositorySpec

--- src/test/scala/ValueRepositorySpec.scala
+++ src/test/scala/ValueRepositorySpec.scala
@@ -1,111 +1,92 @@
 import ValueRepository._
 import akka.actor.testkit.typed.scaladsl.ActorTestKit
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.cluster.VectorClock
+import akka.util.Timeout
 import org.scalatest.BeforeAndAfter
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import scala.collection.immutable.TreeMap
+import scala.concurrent.Await.result
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class ValueRepositorySpec extends AnyFlatSpec with Matchers with BeforeAndAfter {
   val testKit: ActorTestKit = ActorTestKit()
 
+  implicit val system: ActorSystem[Nothing] = testKit.system
+  implicit val timeout: Timeout = testKit.timeout
+
+  import akka.actor.typed.scaladsl.AskPattern._
+
   it should "add a value" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myValue"), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myValue"), _: ActorRef[Response])), 1 seconds) should be(OK)
   }
 
   it should "read an empty value" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val readProbe = testKit.createTestProbe[Option[Value]]()
 
-    valueRepository ! GetValueByKey("myKey", readProbe.ref)
-
-    readProbe.expectMessage(None)
+    result(valueRepository.ask(GetValueByKey("myKey", _: ActorRef[Option[Value]])), 1 seconds) should be(None)
   }
 
   it should "read an added value" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myValue"), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myValue"), _: ActorRef[Response])), 1 seconds) should be(OK)
 
-    val readProbe = testKit.createTestProbe[Option[Value]]()
-
-    valueRepository ! GetValueByKey("myKey", readProbe.ref)
-
-    readProbe.expectMessage(Option(Value("myKey", "myValue")))
+    result(valueRepository.ask(GetValueByKey("myKey", _: ActorRef[Option[Value]])), 1 seconds) should be(Option(Value("myKey", "myValue")))
   }
 
   it should "remove a value" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myValue"), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myValue"), _: ActorRef[Response])), 1 seconds) should be(OK)
 
-    valueRepository ! RemoveValue("myKey", probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(RemoveValue("myKey", _: ActorRef[Response])), 1 seconds) should be(OK)
+
+    result(valueRepository.ask(GetValueByKey("myKey", _: ActorRef[Option[Value]])), 1 seconds) should be(None)
   }
 
   it should "return an error when removing a value if it does not extist" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! RemoveValue("myKey", probe.ref)
-    probe.expectMessage(KO("Not Found"))
+    result(valueRepository.ask(RemoveValue("myKey", _: ActorRef[Response])), 1 second) should be(KO("Not Found"))
   }
 
   it should "update a value if the version is newer" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node"-> 0))), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 0))), _: ActorRef[Response])), 1 second) should be(OK)
 
-    valueRepository ! AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 1))), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 1))), _: ActorRef[Response])), 1 second) should be(OK)
   }
 
   it should "not update a value if the version is equal" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 0))), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 0))), _: ActorRef[Response])), 1 second) should be(OK)
 
-    valueRepository ! AddValue(Value("myKey", "myAnotherValue", new VectorClock(TreeMap("Node" -> 0))), probe.ref)
-    probe.expectMessage(KO("Version too old"))
+    result(valueRepository.ask(AddValue(Value("myKey", "myAnotherValue", new VectorClock(TreeMap("Node" -> 0))), _: ActorRef[Response])), 1 second) should be(KO("Version too old"))
   }
 
   it should "not update a value if the version is older" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 1))), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myOtherValue", new VectorClock(TreeMap("Node" -> 1))), _: ActorRef[Response])), 1 second) should be(OK)
 
-    valueRepository ! AddValue(Value("myKey", "myAnotherValue", new VectorClock(TreeMap("Node" -> 0))), probe.ref)
-    probe.expectMessage(KO("Version too old"))
+    result(valueRepository.ask(AddValue(Value("myKey", "myAnotherValue", new VectorClock(TreeMap("Node" -> 0))), _: ActorRef[Response])), 1 second) should be(KO("Version too old"))
   }
 
   it should "clear the repository" in {
     val valueRepository = testKit.spawn(ValueRepository())
-    val probe = testKit.createTestProbe[Response]()
 
-    valueRepository ! AddValue(Value("myKey", "myValue"), probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(AddValue(Value("myKey", "myValue"), _: ActorRef[Response])), 1 second) should be(OK)
 
-    valueRepository ! ClearValues(probe.ref)
-    probe.expectMessage(OK)
+    result(valueRepository.ask(ClearValues(_: ActorRef[Response])), 1 second) should be(OK)
 
-    val readProbe = testKit.createTestProbe[Option[Value]]()
-
-    valueRepository ! GetValueByKey("myKey", readProbe.ref)
-    readProbe.expectMessage(None)
+    result(valueRepository.ask(GetValueByKey("myKey", _: ActorRef[Option[Value]])), 1 second) should be(None)
   }
 }
