cb0fd3ec5b9a149c342c3d693d4d5b311284d885
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-03-26 14:51:21 +0100

    Fix HttpSpec

--- src/test/scala/HttpSpec.scala
+++ src/test/scala/HttpSpec.scala
@@ -21,7 +21,7 @@ class HttpSpec extends AnyWordSpec with BeforeAndAfterAll with Matchers with Sca
 
   override def createActorSystem(): actor.ActorSystem = testKit.system.toClassic
 
-  val valueRepository: ActorRef[ValueRepository.Command] = testKit.spawn(ValueRepository(Map.empty, ""))
+  val valueRepository: ActorRef[ValueRepository.Command] = testKit.spawn(ValueRepository(""))
   lazy val routes: Route = new ExternalRoutes(valueRepository).theValueRoutes
 
   "The service" should {
